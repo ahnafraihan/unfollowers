dca3bd425077f972f65a11da2c6ebf05c99dffb1
Ahnaf Raihan <ahnaf@google.com>
2023-11-17 21:49:42 -0500

    add comments

--- unfollowers.js
+++ unfollowers.js
@@ -1,9 +1,13 @@
+// access array of account objects and reduce to usernames
 const getUsers = accounts => accounts.map(f => f.string_list_data[0].value).filter(f => f);
 
+// compare follower & following data via instagram and return list of unfollowers
 const getUnfollowers = () => {
+  // get files for following & followers
   const following_file = document.getElementById('following_file').files[0];
   const followers_file = document.getElementById('followers_file').files[0];
 
+  // confirm files were uploaded
   if (!following_file) {
     alert('upload following.json');
     return;
@@ -14,20 +18,27 @@ const getUnfollowers = () => {
     return;
   }
 
+  // instantiate file readers
   const following_reader = new FileReader();
   const follower_reader = new FileReader();
 
   following_reader.onload = function() {
+    // parse following into JSON object
     const followingJSON = JSON.parse(this.result);
 
     follower_reader.onload = function() {
+      // parse followers into JSON object
       const followersJSON = JSON.parse(this.result);
       
+      // get list of usernames (following & followers)
       const following = getUsers(followingJSON.relationships_following);
       const followers = getUsers(followersJSON);
 
+      // get reverse intersection of lists to find unfollowers
       const unfollowers = following.filter(x => !followers.includes(x));
-      document.getElementById('result').innerHTML = unfollowers.join(', ');
+
+      // display unfollowers
+      document.getElementById('unfollowers').innerHTML = unfollowers.join(', ');
     };
     follower_reader.readAsText(followers_file);
   };
